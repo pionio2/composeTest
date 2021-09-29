f4bb3098372ec33c0f961c00b3ff40d6bda0028c
Doohyun <pionio@nate.com>
2021-09-30 01:21:58 +0900

    Custom Layout 을 이용한 Modifier

--- app/src/main/java/com/example/composetest/MainActivity.kt
+++ app/src/main/java/com/example/composetest/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.composetest
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.example.composetest.ui.theme.ComposeTestTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -22,10 +31,38 @@ class MainActivity : ComponentActivity() {
             ComposeTestTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    SideEffectScreen()
+//                    SideEffectScreen()
+                    LayoutModifierSample()
                 }
             }
         }
+    }
+}
+
+fun Modifier.firstBaselineToTop(firstBaselineToTop: Dp) = layout { measurable, constraints ->
+    // Composable 측정
+    val placeable = measurable.measure(constraints)
+
+    // 측정값(pixel 3 기준 -  width:163px | height:59px
+
+    // Check the composable has a first baseline
+    check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+    val firstBaseline = placeable[FirstBaseline]
+
+    // 측정값 - firstBaseline: 47px
+
+    // Height of the composable with padding - first baseline
+    val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+    val height = placeable.height + placeableY
+
+    // 측정값 - firstBaselineToTop.roundToPx(): 88px, height:100
+
+    Log.e("composeTest","pw:${placeable.width} | ph:${placeable.height} | firstBaseline:$firstBaseline |  firstBaselineToTop.roundToPx():${ firstBaselineToTop.roundToPx()} | placeableY:$placeableY " +
+            "| height:$height")
+
+    layout(placeable.width, height) {
+        // Where the composable gets placed
+        placeable.placeRelative(0, placeableY)
     }
 }
 
@@ -34,10 +71,18 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+@Composable
+fun LayoutModifierSample() {
+    Column {
+        Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
+        Text("Hi there!", Modifier.padding(top = 32.dp))
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeTestTheme {
-        SideEffectScreen()
+        LayoutModifierSample()
     }
 }
