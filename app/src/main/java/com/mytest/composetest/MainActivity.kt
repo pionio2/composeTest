a9f94671c2916045fe9ef52dd384176235579b1c
DoohyunKim <doohyun.kim@sk.com>
2022-11-19 21:32:09 +0900

    [Compose BOM] 적용

--- app/src/main/java/com/mytest/composetest/MainActivity.kt
+++ app/src/main/java/com/mytest/composetest/MainActivity.kt
@@ -16,14 +16,13 @@ import androidx.compose.foundation.layout.*
 
 import androidx.compose.material.*
 import androidx.compose.runtime.*
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
-
 import androidx.compose.ui.tooling.preview.Preview
+
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
